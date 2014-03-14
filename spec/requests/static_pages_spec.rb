c6ec10617077ec89ffd3482533e158b241991885
vlad <vlad_tere@yahoo.com>
2014-03-14 09:52:06 +0100

    help page

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -10,10 +10,16 @@ describe "Static_pages" do
 	end
 
 	describe "Help page" do
-		it "should have the content 'Help'" do
+		it "should have the content 'Help' " do
 			visit 'static_pages/help'
 			expect(page).to have_content('Help')
 		end
 	end
 
+	describe "About page" do
+		it "should have the content 'About us'" do
+			visit '/static_pages/about'
+			expect(page).to have_content('About Us')
+		end
+	end
 end
