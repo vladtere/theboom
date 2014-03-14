81cf992768ef1b11f0af97c4e582250913991f97
vlad <vlad_tere@yahoo.com>
2014-03-14 11:22:39 +0100

    Finish static pages

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -7,12 +7,20 @@ describe "Static_pages" do
 			visit '/static_pages/home'
 			expect(page).to have_content('Sample App')
 		end
+		it "should have the right title" do
+			visit '/static_pages/home'
+			expect(page).to have_title('Home')
+		end
 	end
 
 	describe "Help page" do
-		it "should have the content 'Help' " do
-			visit 'static_pages/help'
-			expect(page).to have_content('Help')
+		it "should have the content 'help' " do
+			visit '/static_pages/help'
+			expect(page).to have_content('help')
+		end
+		it "should have the right title" do
+			visit '/static_pages/help'
+			expect(page).to have_title('Help')
 		end
 	end
 
@@ -20,6 +28,10 @@ describe "Static_pages" do
 		it "should have the content 'About us'" do
 			visit '/static_pages/about'
 			expect(page).to have_content('About Us')
+		end
+		it "should have the right title" do
+			visit '/static_pages/about'
+			expect(page).to have_title('About')
 		end
 	end
 end
